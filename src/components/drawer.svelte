80a9fb914f0eccf1f0904d95ba622c1b4472cec9
PaoloGiraudi <pgiraudi94@gmail.com>
2023-05-05 16:15:49 +0200

    fix palette and buttons

---
+++ src/components/drawer.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { fade, fly } from 'svelte/transition';
+	export let open: Boolean;
+</script>
+
+{#if open}
+	<div data-role="backdrop" on:keypress in:fade={{ duration: 150 }} out:fade={{ duration: 250 }}>
+		<div data-role="dialog" in:fly={{ y: 500, duration: 150 }} out:fly={{ y: 500, duration: 250 }}>
+			<slot />
+		</div>
+	</div>
+{/if}
+
+<style>
+	div[data-role='backdrop'] {
+		position: fixed;
+		display: flex;
+		align-items: flex-end;
+		top: 0;
+		left: 0;
+		min-height: 100%;
+		width: 100%;
+		background-color: hsl(var(--gray-11-hsl) / 10%);
+		backdrop-filter: blur(var(--size-1));
+	}
+	div[data-role='dialog'] {
+		display: flex;
+		padding: var(--size-3);
+		height: var(--size-xs);
+		width: 100%;
+		background-color: var(--surface);
+	}
+</style>
--- src/components/settings.svelte
+++ src/components/settings.svelte
@@ -1,55 +1,18 @@
 <script lang="ts">
 	import { setTheme, theme } from '$lib/theme';
-	import { fade, fly } from 'svelte/transition';
-	export let open: Boolean;
 </script>
 
-{#if open}
-	<div data-role="backdrop" on:keypress in:fade={{ duration: 150 }} out:fade={{ duration: 250 }}>
-		<div data-role="dialog" in:fly={{ y: 500, duration: 150 }} out:fly={{ y: 500, duration: 250 }}>
-			<div data-role="group">
-				<label for="theme-selector">Select theme</label>
-				<button
-					type="button"
-					data-selected={$theme === 'dark'}
-					on:click={() => setTheme('dark')}
-					id="theme-selector"
-				>
-					Dark
-				</button>
-				<button
-					type="button"
-					data-selected={$theme === 'light'}
-					on:click={() => setTheme('light')}
-					id="theme-selector"
-				>
-					Light
-				</button>
-			</div>
-		</div>
-	</div>
-{/if}
+<div data-role="group">
+	<label for="theme-selector">Select theme</label>
+	<button type="button" data-selected={$theme === 'dark'} on:click={() => setTheme('dark')}>
+		Dark
+	</button>
+	<button type="button" data-selected={$theme === 'light'} on:click={() => setTheme('light')}>
+		Light
+	</button>
+</div>
 
 <style>
-	div[data-role='backdrop'] {
-		position: fixed;
-		display: flex;
-		align-items: flex-end;
-		top: 0;
-		left: 0;
-		min-height: 100%;
-		width: 100%;
-		background-color: hsl(var(--gray-11-hsl) / 10%);
-		backdrop-filter: blur(var(--size-1));
-	}
-	div[data-role='dialog'] {
-		display: flex;
-		padding: var(--size-3);
-		height: var(--size-xs);
-		width: 100%;
-		background-color: var(--surface);
-	}
-
 	div[data-role='group'] {
 		gap: var(--size-2);
 		align-items: center;
@@ -61,6 +24,7 @@
 	label {
 		font-size: var(--font-size-2);
 		margin-inline-end: auto;
+		font-weight: var(--font-weight-5);
 	}
 
 	button {
@@ -72,11 +36,12 @@
 		outline: none;
 		transition: none;
 		border-color: var(--text-2);
+		font-weight: var(--font-weight-5);
 	}
 
 	button[data-selected='true'] {
 		cursor: pointer;
-		color: var(--gray-3);
+		color: var(--gray-2);
 		background-color: var(--accent);
 	}
 </style>
