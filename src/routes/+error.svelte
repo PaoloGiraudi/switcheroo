ad49dfb4284fd5ddd47e7c0db12a51fb1c15825f
PaoloGiraudi <lp.web@pm.me>
2023-11-20 15:29:49 +0100

    error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<h1>{$page.status}: {$page.error?.message}</h1>
+<h2>Oops! Something went wrong :(</h2>
+<p>
+  Try to refresh the page or go back to the
+  <a href="/" data-sveltekit-preload-data="hover">Home</a>
+</p>
