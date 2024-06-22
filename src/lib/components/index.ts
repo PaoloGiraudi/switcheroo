cd91955a71b37b4ac825dcd5ca571538a5a562a1
PaoloGiraudi <lp.web@pm.me>
2024-06-22 15:27:53 +0200

    add info page and fix some styling

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,8 +1,6 @@
-export { default as Card } from './card.svelte';
 export { default as Field } from './field.svelte';
 export { default as Message } from './message.svelte';
 export { default as ConversionField } from './conversion-field.svelte';
-export { default as Info } from './info.svelte';
 export { default as Input } from './input.svelte';
 export { default as Navbar } from './navbar.svelte';
 export { default as Select } from './select.svelte';
